42d2409f8041b1a8ed27a7750b1e5aa533cdb05e
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-07-08 00:55:50 -0400

    feat: placement and opacity of menu - LETS GO

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,8 +1,8 @@
 export default function Menu() {
   return (
-    <nav class="text-white bg-night-black rounded-lg w-full py-5 pl-2">
-      <ul>
-        <li>
+    <nav class="text-3xl text-white bg-night-black rounded-lg w-full py-5 pl-2 bg-opacity-50">
+      <ul class="flex flex-col gap-6 opacity-88">
+        <li class="">
           <a href="/experience">Experience</a>
         </li>
         <li>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,10 @@ import Menu from "../components/menu";
 
 export default function Home() {
   return (
-    <main class="flex flex-row ">
+    <main class="flex flex-row h-screen items-center">
       <div class="w-1/5"></div>
-      <div class="w-5/6">
-        <div class="w-72 flex flex-col items-center">
+      <div class="w-4/5">
+        <div class="w-72 flex flex-col items-center gap-1.5">
           <h1 class="font-medium text-white text-5xl">Jakob Strobl</h1>
           <Menu></Menu>
         </div>
