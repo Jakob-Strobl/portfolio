478560c7af22d70b212666714c62190b177acbd3
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-07-09 00:09:47 -0400

    define project version as esbuild variable and render to page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
         <div class="w-72 flex flex-col items-center gap-1.5">
           <h1 class="font-medium text-white text-5xl">Jakob Strobl</h1>
           <Menu></Menu>
+          <p class="text-gray-300">{process.env.PROJECT_VERSION}</p>
         </div>
       </div>
     </>
