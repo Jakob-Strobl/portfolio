908515f02ef6e216cb666c0eab89786fb110edcd
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-07-08 22:06:54 -0400

    fix: move main tag to base layout for center children content by default

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import Menu from "../components/menu";
 
 export default function Home() {
   return (
-    <main class="flex flex-row h-screen items-center">
+    <>
       <div class="w-1/5"></div>
       <div class="w-4/5">
         <div class="w-72 flex flex-col items-center gap-1.5">
@@ -10,6 +10,6 @@ export default function Home() {
           <Menu></Menu>
         </div>
       </div>
-    </main>
+    </>
   );
 }
