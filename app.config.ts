e54cc3d2d349e6a134c1f3c0e4788b6acaf2d102
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-07-09 00:09:47 -0400

    feat: define project version as esbuild variable and render to page

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "@solidjs/start/config";
 import glsl from "vite-plugin-glsl";
 import solid from "vite-plugin-solid";
+import Package from "./package.json";
 
 export default defineConfig({
   vite: {
     plugins: [glsl(), solid({ ssr: true })],
     resolve: {
       conditions: ["development", "browser"],
+    },
+    esbuild: {
+      define: {
+        "process.env.PROJECT_VERSION": JSON.stringify(Package.version),
+      },
     },
   },
   server: {
