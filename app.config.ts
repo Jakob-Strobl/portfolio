291bfa33744473ca68aa51f7cbb8bc80ff18c113
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-07-14 02:42:36 -0400

    yeah I don't know why this settings don't work for me when it's the example on solid docs

--- app.config.ts
+++ app.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     preset: "cloudflare-pages",
     rollupConfig: {
-      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+      external: [],
     },
   },
 });
