e30e1f337a7f039d47b5f34454d8fa58ba8c7e84
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-08-01 20:00:45 -0400

    feat: fade-in component and fade in menu buttons (doesn't work on load though sadge)

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,8 +1,15 @@
+import { onMount, createSignal } from "solid-js";
+
 export default function Menu() {
+  const [isReady, setReady] = createSignal(false);
+  onMount(() => {
+    setTimeout(() => setReady(true), 1);
+  });
+
   return (
     <nav class="text-3xl text-white bg-night-black rounded-lg w-full py-5 pl-2 bg-opacity-50">
       <ul class="flex flex-col gap-6 opacity-88">
-        <li>
+        <li class="fade-in delay-150" classList={{ "opacity-0": !isReady() }}>
           <a
             href="/experience"
             class="hover:text-stroke-lg duration-300 transition-text"
@@ -10,7 +17,7 @@ export default function Menu() {
             Experience
           </a>
         </li>
-        <li>
+        <li class="fade-in delay-300" classList={{ "opacity-0": !isReady() }}>
           <a
             href="/contact"
             class="hover:text-stroke-lg duration-300 transition-text"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,22 @@
 import Menu from "../components/menu";
+import { onMount, createSignal } from "solid-js";
 
 export default function Home() {
+  const [isReady, setReady] = createSignal(false);
+  onMount(() => {
+    setTimeout(() => setReady(true), 1);
+  });
+
   return (
     <>
       <div class="w-1/5"></div>
       <div class="w-4/5">
-        <div class="w-72 flex flex-col items-center gap-1.5">
+        <div
+          class="w-72 flex flex-col items-center gap-1.5 fade-in"
+          classList={{
+            "opacity-0": !isReady(),
+          }}
+        >
           <h1 class="font-medium text-white text-5xl">Jakob Strobl</h1>
           <Menu></Menu>
           <p class="text-gray-300">
