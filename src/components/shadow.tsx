d80aa2063e8067f6548fd059984503a716f8d175
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-09-22 22:21:12 -0400

    fix: store shape and add comments for fade-in

--- src/components/shadow.tsx
+++ src/components/shadow.tsx
@@ -14,12 +14,12 @@ interface ShadowProps {
 
 export default function Shadow(props: ShadowProps) {
   const resolved = children(() => props.children);
-  const [isReady, setReady] = createSignal(false);
+  // const [isReady, setReady] = createSignal(false);
   const shadowId = createUniqueId();
   let shadowEl: HTMLDivElement;
 
   onMount(() => {
-    setTimeout(() => setReady(true), 0);
+    // setTimeout(() => setReady(true), 0);
     // use onMount or createEffect to read after connected to DOM
     addShadow(shadowEl);
   });
@@ -31,12 +31,12 @@ export default function Shadow(props: ShadowProps) {
   return (
     <div
       class="rounded-lg w-full p-5 text-white"
-      style={{
-        // Set inline so the browser doesn't try to fade from 100% -> 0% opacity
-        // and then fade back to 100% once ready flag is flipped
-        // This is why the fade looked like it wasn't working. It was starting with fade out.
-        "--tw-bg-opacity": !isReady() ? 0 : 0.5,
-      }}
+      // style={{
+      //   // Set inline so the browser doesn't try to fade from 100% -> 0% opacity
+      //   // and then fade back to 100% once ready flag is flipped
+      //   // This is why the fade looked like it wasn't working. It was starting with fade out.
+      //   "--tw-bg-opacity": !isReady() ? 0 : 0.5,
+      // }}
       ref={(el) => (shadowEl = el)}
       data-shadow={shadowId}
     >
