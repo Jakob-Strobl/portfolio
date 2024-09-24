f3b135b24f51aa357c6763f4fc06b3801d1510e3
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-09-23 22:21:14 -0400

    feat: compute client side bounding box on resize

--- src/components/umbra.tsx
+++ src/components/umbra.tsx
@@ -1,5 +1,4 @@
-import { createMemo } from "solid-js";
-import type { JSX } from "solid-js";
+import { createMemo, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 
 interface UmbraState {
@@ -8,15 +7,23 @@ interface UmbraState {
 }
 
 export default function Umbra() {
-  const rect = createMemo(
-    () =>
+  // const clientRect = createSignal(state.shadow[0]?.getBoundingClientRect())
+  const clientRect = createMemo(() => {
+    return (
       state.shadow[0]?.getBoundingClientRect() ?? {
         width: 10,
         height: 10,
         top: 0,
-        left: 0,
-      },
-  );
+        left: 1920,
+      }
+    );
+  });
+
+  onMount(() => {
+    window.addEventListener("resize", () => {
+      setState({ shadows: [...state.shadows] });
+    });
+  });
 
   // TODO currently hardcoded for one shadow at a time
   // TODO make flexible with 1->N shadow transitions and N->K shadows
@@ -26,13 +33,17 @@ export default function Umbra() {
       class="
         absolute -z-10 transition-rect rounded-lg
         bg-opacity-50 bg-night-black 
-        fade-in-bg duration-1000 delay-75
+        fade-in-bg duration-1000
       "
       style={{
-        width: `${rect().width}px`,
-        height: `${rect().height}px`,
-        top: `${rect().top}px`,
-        left: `${rect().left}px`,
+        width: `${clientRect().width}px`,
+        height: `${clientRect().height}px`,
+        top: `${clientRect().top}px`,
+        left: `${clientRect().left}px`,
+        // TODO avoid transition on resize
+        // bottom and right are not transitioned
+        // bottom: `${clientRect().bottom}px`,
+        // right: `${clientRect().right}px`
       }}
     ></div>
   );
