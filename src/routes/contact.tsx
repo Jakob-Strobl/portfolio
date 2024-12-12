13001e3c35c8769efcfd44c9157fb8aa477a6762
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-12-12 02:19:44 -0500

    feat(contact): add linkedin and github links with matched logo dimensions

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -4,13 +4,36 @@ export default function Contact() {
   return (
     <div>
       <Shadow>
-        <h1 class="text-2xl text-white justify-self-center">Working on it!</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        {/* padding's screem size breakpoints should match the image width's breakpoints */}
+        <div class="flex items-end p-6 sm:p-8 xl:p-12">
+          <div id="github">
+            <a
+              class="flex flex-col items-center gap-8"
+              href="https://github.com/Jakob-Strobl"
+              target="_blank"
+            >
+              <img
+                class="github-normalized-size"
+                src="./images/Github-octocat.png"
+              />
+              <p>GitHub</p>
+            </a>
+          </div>
+
+          <div id="linkedin">
+            <a
+              class="flex flex-col items-center gap-8"
+              href="https://www.linkedin.com/in/jakob-strobl"
+              target="_blank"
+            >
+              <img
+                class="linkedin-normalized-size"
+                src="./images/LinkedIn.png"
+              />
+              <p>LinkedIn</p>
+            </a>
+          </div>
+        </div>
         <a href="/" class="hover:text-stroke-lg duration-300 transition-text">
           back
         </a>
