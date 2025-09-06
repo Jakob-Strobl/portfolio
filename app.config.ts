e5ba61964b588a6d3338c4230b43f7a26ee79f4f
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-09-05 20:17:35 -0400

    fix(dependencies): update all dependencies to latest versions

--- app.config.ts
+++ app.config.ts
@@ -2,23 +2,17 @@ import { defineConfig } from "@solidjs/start/config";
 import glsl from "vite-plugin-glsl";
 import solid from "vite-plugin-solid";
 import Package from "./package.json";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [glsl(), solid({ ssr: true })],
-    resolve: {
-      conditions: ["development", "browser"],
-    },
+    plugins: [tailwindcss(), glsl(), solid({ ssr: true })],
+    resolve: { conditions: ["development", "browser"] },
     esbuild: {
       define: {
         "process.env.PROJECT_VERSION": JSON.stringify(Package.version),
       },
     },
   },
-  server: {
-    preset: "cloudflare-pages",
-    rollupConfig: {
-      external: [],
-    },
-  },
+  server: { preset: "cloudflare-pages", rollupConfig: { external: [] } },
 });
