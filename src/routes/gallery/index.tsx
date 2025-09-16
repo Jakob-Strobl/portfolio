eec6876e6d174a33c291bb2608a786ecda16d8fb
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-09-16 12:56:33 -0400

    feat(layouts): add linear layout poc on gallery
    All transitions still work!

--- src/routes/gallery/index.tsx
+++ src/routes/gallery/index.tsx
@@ -1,3 +1,4 @@
+import LinearLayout from "~/layouts/linear-layout";
 import Shadow from "../../components/shadow/shadow";
 import PhotoSunsetGlimmer from "./2020/sunset-glimmer";
 import PhotoViewOfModernHimeji from "./2020/view-of-modern-himeji";
@@ -10,26 +11,19 @@ export default function Gallery() {
     // Width of gap is 4 (4 gaps per 5 images) = 16
     // Width of shadow padding is p-5 = p-5 * 2 both sides = 10
     // Sum: 236
-    // TODO [ ] Create a layout based on this left-center-right structure that starts offset from the top
+    // TODO [X] Create a layout based on this left-center-right structure that starts offset from the top
     // Right now mt-120 looks good on desktop (1440p full + half screen).
     // Will need to change layout for mobile portriat
-    <>
-      {/* LEFT Gutter */}
-      <div class="w-1/5 h-full flex justify-end items-start px-2 mt-120">
-        <div class="max-w-36 w-fit fixed">
-          <Shadow warmupDelayMs={0} contentFadeInDelayMs={500} fixed>
-            <a
-              href="/"
-              class="hover:text-shadow-lg duration-300 transition-text flex gap-1 items-center"
-            >
-              <ArrowBigLeft size={20} /> Home
-            </a>
-          </Shadow>
-        </div>
-      </div>
-      {/* CENTER Content */}
-      <div class="w-3/5 h-full mt-120">
-        <div class="flex flex-col gap-8 mb-4">
+    <LinearLayout
+      navBack={
+        <Shadow warmupDelayMs={0} contentFadeInDelayMs={500} fixed>
+          <a href="/" class="hover:text-shadow-lg duration-300 transition-text flex gap-1 items-center">
+            <ArrowBigLeft size={20} /> Home
+          </a>
+        </Shadow>
+      }
+      content={
+        <>
           <Shadow warmupDelayMs={125} contentFadeInDelayMs={500}>
             <div class="flex flex-col gap-4">
               <h1 class="text-4xl text-white">2025</h1>
@@ -52,10 +46,8 @@ export default function Gallery() {
               </div>
             </div>
           </Shadow>
-        </div>
-      </div>
-      {/* RIGHT Gutter */}
-      <div class="w-1/5 h-full"></div>
-    </>
+        </>
+      }
+    ></LinearLayout>
   );
 }
