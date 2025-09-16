9a1f0e2184cc4ca4ec7f612196dcbc44809bccb4
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-09-16 19:07:32 -0400

    feat(gallery): darken thumbnail photos until hover

--- src/components/gallery-photo.tsx
+++ src/components/gallery-photo.tsx
@@ -6,7 +6,11 @@ export interface GalleryPhotoProps {
 export default function GalleryPhoto(props: GalleryPhotoProps) {
   const isThumbnailView = props.thumbnailView ?? true;
   return (
-    <div class={isThumbnailView ? "w-full 2xl:h-72 md:h-54" : ""}>
+    <div
+      class={
+        isThumbnailView ? "w-full 2xl:h-72 md:h-54 opacity-76 hover:opacity-100 transition-opacity duration-300" : ""
+      }
+    >
       <img class="w-full h-full rounded-sm object-cover" src={props.uri}></img>
     </div>
   );
