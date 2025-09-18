f91df401520d7a0e05568cb6fd028866e37de8e8
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-09-18 18:43:34 -0400

    feat(layouts): add navback prop to full-photo

--- src/layouts/full-photo-layout.tsx
+++ src/layouts/full-photo-layout.tsx
@@ -1,13 +1,22 @@
+import { JSX } from "solid-js";
 import GalleryPhoto from "../components/gallery-photo";
 import Shadow from "../components/shadow/shadow";
 import { PhotoResource } from "../types/photo-resource";
 import ArrowBigLeft from "lucide-solid/icons/arrow-big-left";
 import ArrowBigRight from "lucide-solid/icons/arrow-big-right";
+// import Home from "lucide-solid/icons/home"
 
 export interface FullPhotoLayoutProps {
+  navBack?: JSX.Element;
   resource: PhotoResource;
   photoCollection?: PhotoResource[];
 }
+
+const navBackFallback = () => (
+  <a href="/">
+    <ArrowBigLeft size={20} /> Home
+  </a>
+);
 
 export default function FullPhotoLayout(props: FullPhotoLayoutProps) {
   return (
@@ -17,30 +26,27 @@ export default function FullPhotoLayout(props: FullPhotoLayoutProps) {
         {/* TODO(Investigate): Some interesting pre-defined width/heights via aspect-ratio */}
         {/* Top Row - Full Photo */}
         <div class="h-[calc(100vh---spacing(38))]">
-          <Shadow>
+          <Shadow warmupDelayMs={0}>
             <GalleryPhoto uri={props.resource.uri} thumbnailView={false}></GalleryPhoto>
           </Shadow>
         </div>
         {/* Bottom Row  - Collection Navigation */}
         <div class="flex gap-2 xl:h-24 lg:h-20 md:h-16 px-16 items-center">
           <div class="">
-            <Shadow>
+            <Shadow warmupDelayMs={500}>
               <div class="hover:text-shadow-lg duration-300 transition-text *:flex *:gap-1 *:items-center">
-                {/* TODO: Refactor into navback prop like other layouts */}
-                <a href="/">
-                  <ArrowBigLeft size={20} /> Home
-                </a>
+                {props.navBack ?? navBackFallback()}
               </div>
             </Shadow>
           </div>
           <div class="h-24 w-full">
-            <Shadow>
+            <Shadow warmupDelayMs={250}>
               {/* Shadow padding is 5 top and bottom, so calc(h-24 - 5 (spacing) * 2) = h-20 - 10 = h-14 */}
               <div class="h-14"></div>
             </Shadow>
           </div>
           <div class="">
-            <Shadow>
+            <Shadow warmupDelayMs={500}>
               <div class="hover:text-shadow-lg duration-300 transition-text *:flex *:gap-1 *:items-center">
                 <a href="/">
                   Next <ArrowBigRight size={20} />
