8294df24d3b010aea84a23193edecc71ed4651e8
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-09-24 13:50:34 -0400

    style(timeline): layout is mobile ready and more responsive

--- src/routes/gallery/collections/himeji/+photos.tsx
+++ src/routes/gallery/collections/himeji/+photos.tsx
@@ -6,7 +6,7 @@ export const photoCollection: PhotoResource[] = [{ uri: "fASTqmqQqXI16uEgpeEl8Os
 export const PhotoCollection = () => (
   <div class="flex flex-col gap-4">
     <h1 class="text-4xl text-white">Japan - Himeji</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
       {photoCollection.map((photo) => (
         <GalleryPhoto collectionDirName="himeji" uriKey={photo.uri} />
       ))}
