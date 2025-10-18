0b6e5dc590d0c14475826bdcb2f1d9082024dc4b
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-10-17 20:05:02 -0400

    fix(shadow): clamp negative clientY on elastic scroll on ios

--- src/components/shadow/actions.ts
+++ src/components/shadow/actions.ts
@@ -155,7 +155,8 @@ export const clearRemovedShadows = () => {
 export const forceRecalculateShadowClientRects = () => {
   state.shadows.forEach((shadow) => {
     const clientRect = shadow.shadowedEl.getBoundingClientRect();
-    shadow.setPosition({ x: clientRect.x, y: clientRect.y });
+    const clientY = Math.max(0, clientRect.y); // prevent negative y which causes issues with fixed shadows
+    shadow.setPosition({ x: clientRect.x, y: clientY });
     shadow.setDimensions({ x: clientRect.width, y: clientRect.height });
     shadow.setShadowState("moving"); // removes borders until the transition after the recalculated rects is "warm"
   });
