504c908e5bcb5eb980609a87df36eb95acf3615a
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-10-18 00:11:37 -0400

    feat(waves): render behind navbar and notch on ios

---
+++ src/actions/device-actions.ts
@@ -0,0 +1,8 @@
+export function isIOS() {
+  return /iPad|iPhone|iPod/i.test(navigator.userAgent) || navigator.maxTouchPoints > 1;
+}
+
+export function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    || navigator.maxTouchPoints > 1;
+}
