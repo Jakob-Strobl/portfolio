8b45d5dd8d77a28074265da08a5bccc534eb3b01
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-12-13 01:23:04 -0500

    feat(layout): shrink back button on smaller screens

--- src/layouts/timeline-layout.tsx
+++ src/layouts/timeline-layout.tsx
@@ -81,7 +81,7 @@ export default function TimelineLayout(props: TimelineLayout) {
         style={{ opacity: isReady() ? 1 : 0 }}
       ></div>
       {/* LEFT Gutter */}
-      <div class={`md:w-1/5 min-w-26 h-full flex flex-col items-end justify-start px-2  ${topMargin}`}>
+      <div class={`md:w-1/5 w-2/12 h-full flex flex-col items-end justify-start px-2  ${topMargin}`}>
         <div class="fixed md:max-w-3/4 max-w-1/2 w-fit">
           <Shadow warmupDelayMs={0} contentFadeInDelayMs={500} fixed>
             <div class="hover:text-shadow-lg duration-300 transition-text *:flex *:gap-1 *:items-center">
@@ -97,14 +97,14 @@ export default function TimelineLayout(props: TimelineLayout) {
         </div>
       </div>
       {/* CENTER Content */}
-      <div class={`w-3/5 h-full ${topMargin}`}>
+      <div class={`w-10/12 md:w-3/5 h-full ${topMargin}`}>
         <div ref={contentContainerRef} class={`flex flex-col ${contentGap} mb-4`}>
           {props.content}
           <div class="max-h-[48vh] min-h-[16vh] h-dvh"></div>
         </div>
       </div>
       {/* RIGHT Gutter */}
-      <div class={`md:w-1/5 w-0 h-full ${topMargin}`}>{props.focus}</div>
+      <div class={`w-2 xs:w-4 md:w-1/5 h-full ${topMargin}`}>{props.focus}</div>
     </div>
   );
 }
