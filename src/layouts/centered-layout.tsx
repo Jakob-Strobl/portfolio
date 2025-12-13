3c073264cc866b5be9dfcdbf64882ff1bba4f8c1
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-12-13 03:30:41 -0500

    fix(contact): firefox blank screen due to icon import path

--- src/layouts/centered-layout.tsx
+++ src/layouts/centered-layout.tsx
@@ -1,18 +1,25 @@
 import { LinearLayoutProps } from "./linear-layout";
 import Shadow from "../components/shadow/shadow";
-export default function CenteredLayout(props: LinearLayoutProps) {
+import { children, JSX } from "solid-js";
+
+export interface CenteredLayoutProps {
+  children: JSX.Element | JSX.ArrayElement;
+  navBack?: () => JSX.Element;
+}
+export default function CenteredLayout(props: CenteredLayoutProps) {
+  const resolved = children(() => props.children);
   return (
-    <div class={`flex flex-row h-screen items-center justify-center`}>
+    <div class={`flex flex-row h-screen items-center justify-center m-2 ml-4`}>
       {/* CENTER Content */}
-      <div class="flex flex-row gap-2 mb-4 place-content-center relative">
-        <div class="absolute w-fit right-[100%] mr-2">
+      <div class="flex flex-row gap-2 place-content-center relative w-full">
+        <div class="w-fit">
           <Shadow warmupDelayMs={0} contentFadeInDelayMs={500} fixed>
             <div class="hover:text-shadow-lg duration-300 transition-text *:flex *:gap-1 *:items-center">
               {props.navBack?.()}
             </div>
           </Shadow>
         </div>
-        {props.content}
+        {resolved()}
       </div>
     </div>
   );
--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -3,7 +3,7 @@ import { copyToClipboard } from "../actions/clipboard-actions";
 import NotifyBubble from "../components/notify-bubble";
 import Shadow from "../components/shadow/shadow";
 import { A } from "@solidjs/router";
-import { ArrowBigLeft } from "lucide-solid";
+import ArrowBigLeft from "lucide-solid/icons/arrow-big-left";
 import CenteredLayout from "../layouts/centered-layout";
 
 export default function Contact() {
@@ -26,63 +26,58 @@ export default function Contact() {
           <span class="hidden sm:inline">Home</span>
         </A>
       )}
-      content={
-        <Shadow warmupDelayMs={0} contentFadeInDelayMs={400}>
-          {/* padding's screem size breakpoints should match the image width's breakpoints */}
-          <div class="flex flex-col p-6 sm:p-8 xl:p-12 w-full ">
-            <div>
-              <p class="text-3xl">Connect with me:</p>
+    >
+      <Shadow warmupDelayMs={0} contentFadeInDelayMs={400}>
+        {/* padding's screem size breakpoints should match the image width's breakpoints */}
+        <div class="flex flex-col p-6 sm:p-8 xl:p-12 w-full ">
+          <div>
+            <p class="text-3xl">Connect with me:</p>
+          </div>
+          <div class="flex gap-8 p-8 sm:gap-12 sm:p-12 xl:gap-16 xl:p-16">
+            <div id="github">
+              <a class="group flex flex-col items-center gap-8" href="https://github.com/Jakob-Strobl" target="_blank">
+                <img class="github-normalized-size" src="./images/Github-octocat.png" />
+                <p class="group-hover:text-shadow-lg duration-300 transition-text">GitHub</p>
+              </a>
             </div>
-            <div class="flex gap-8 p-8 sm:gap-12 sm:p-12 xl:gap-16 xl:p-16">
-              <div id="github">
-                <a
-                  class="group flex flex-col items-center gap-8"
-                  href="https://github.com/Jakob-Strobl"
-                  target="_blank"
-                >
-                  <img class="github-normalized-size" src="./images/Github-octocat.png" />
-                  <p class="group-hover:text-shadow-lg duration-300 transition-text">GitHub</p>
-                </a>
-              </div>
-              <div id="linkedin">
-                <a
-                  class="group flex flex-col items-center gap-8"
-                  href="https://www.linkedin.com/in/jakob-strobl"
-                  target="_blank"
-                >
-                  <img class="linkedin-normalized-size select-none" src="./images/LinkedIn.png" />
-                  <p class="group-hover:text-shadow-lg duration-300 transition-text">LinkedIn</p>
-                </a>
-              </div>
+            <div id="linkedin">
+              <a
+                class="group flex flex-col items-center gap-8"
+                href="https://www.linkedin.com/in/jakob-strobl"
+                target="_blank"
+              >
+                <img class="linkedin-normalized-size select-none" src="./images/LinkedIn.png" />
+                <p class="group-hover:text-shadow-lg duration-300 transition-text">LinkedIn</p>
+              </a>
             </div>
-            <p class="text-xl">
-              Contact me directly:
-              <span class="flex mt-4 sm:mt-0 sm:inline justify-center">
-                <span
-                  class="
+          </div>
+          <p class="text-xl">
+            Contact me directly:
+            <span class="flex sm:mt-0 sm:inline sm:justify-center">
+              <span
+                class="
                   sm:ml-4 inline-block                   
                   text-night-300 text-center whitespace-nowrap break-normal
                   cursor-pointer select-none
                   border-b-2 border-gray-300/60 hover:border-night-300/90
                   transition-colors duration-300 ease-in-out
                 "
-                  onclick={clickEmailHandler}
-                  title="Click to copy email to clipboard"
-                  aria-label="Click to copy email to clipboard"
-                >
-                  {handle}
-                  <span class="text-white p-2"> at </span>jstrobl.com
-                  {showClipboardBubble() != undefined && (
-                    <NotifyBubble offset={{ x: 0, y: -8 }} originEvent={showClipboardBubble()}>
-                      <p>Copied to clipboard!</p>
-                    </NotifyBubble>
-                  )}
-                </span>
+                onclick={clickEmailHandler}
+                title="Click to copy email to clipboard"
+                aria-label="Click to copy email to clipboard"
+              >
+                {handle}
+                <span class="text-white p-2"> at </span>jstrobl.com
+                {showClipboardBubble() != undefined && (
+                  <NotifyBubble offset={{ x: 0, y: -8 }} originEvent={showClipboardBubble()}>
+                    <p>Copied to clipboard!</p>
+                  </NotifyBubble>
+                )}
               </span>
-            </p>
-          </div>
-        </Shadow>
-      }
-    ></CenteredLayout>
+            </span>
+          </p>
+        </div>
+      </Shadow>
+    </CenteredLayout>
   );
 }
